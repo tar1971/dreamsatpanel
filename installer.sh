014b7fddd9762739975eaf58298453371870a204
tar1971 <96209005+tar1971@users.noreply.github.com>
2022-05-07 03:05:23 +0200

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 ######################################################################################
-## Command=wget https://raw.githubusercontent.com/tarekzoka/dreamsatpanel/main/installer.sh -O - | /bin/sh
+## Command=wget https://raw.githubusercontent.com/tarekzoka/ajpanel/main/installer.sh -O - | /bin/sh
 ##
 ###########################################
 ###########################################
 #!/bin/sh
 echo
 
-opkg install --force-overwrite  https://github.com/tarekzoka/dreamsatpanel/blob/main/enigma2-plugin-extensions-dreamsatpanel_1.3_all.ipk?raw=true
-echo ". >>>>         RESTARING     <<<<"
-echo "**********************************************************************************"
+opkg install --force-overwrite  https://github.com/tarekzoka/ajpanel/blob/main/enigma2-plugin-extensions-ajpanel_v5.0.1_all.ipk?raw=true
 wait
-reboot
+#!/bin/sh
+#
+
+wget -O /tmp/ajpanel_v5.1.0_all.deb "https://github.com/tarekzoka/ajpanel/blob/main/enigma2-plugin-extensions-ajpanel_v5.1.0_all.deb?raw=true"
+wait
+apt-get update ; dpkg -i /tmp/*.deb ; apt-get -y -f install
+wait
+dpkg -i --force-overwrite /tmp/*.deb
+wait
 sleep 2;
-exit 0
