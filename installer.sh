a207ca4ccb6182364beee97a40f243a748a56b29
tar1971 <96209005+tar1971@users.noreply.github.com>
2022-05-07 03:17:36 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ######################################################################################
-## Command=wget https://raw.githubusercontent.com/tarekzoka/dreamsatpanel/main/installer.sh -O - | /bin/sh
+## Command=wget https://raw.githubusercontent.com/tar1971/dreamsatpanel/main/installer.sh -O - | /bin/sh
 ##
 ###########################################
 ###########################################
